29a04ce8c8939e23b42f75336986c9f4d08a3bd3
RehabSayed-G <rehab_hassan@mabrains.com>
2022-12-15 16:34:03 +0200

    adding diode_iv regression to diode_model_reg

--- models/ngspice/testing/regression/diode/model_reg.py
+++ models/ngspice/testing/regression/diode/model_reg.py
@@ -147,9 +147,91 @@ def ext_cv_measured(dev_data_path, device, corners,dev_path):
 
     return df
 
-def run_sim(dirpath, device, length, width, corner, temp):
+def ext_iv_measured(dev_data_path, device, corners,dev_path):
+    # Read Data
+    df = pd.read_excel(dev_data_path)
+
+    dim_df = df[["L (um)", "W (um)"]].copy()
+    dim_df.rename(
+        columns={
+            "L (um)": "length",
+            "W (um)" : "width"
+        },
+        inplace=True,
+    )
+    
+    all_dfs = []
+    
+    loops = dim_df["length"].count()
+    for corner in corners:
+
+        for i in range(0, loops):
+            width = dim_df["width"].iloc[int(i)]
+            length = dim_df["length"].iloc[int(i)]
+
+            if i % 4 == 0:
+                temp = -40
+            elif i % 4 == 1:
+                temp = 25
+            elif i % 4 == 2:
+                temp = 125
+            else:
+                temp = 175
+            
+            leng = []
+            wid = []
+            tempr = []
+            cor = []
+            meas = []
+
+            if i == 0 : 
+                idf = df[["Vn1 (V)", f" |In1(A)| diode_{corner}"]].copy()
+                idf.rename(
+                    columns={
+                        f" |In1(A)| diode_{corner}": "diode_measured",
+                        "Vn1 (V)" : "measured_volt"
+                    },
+                    inplace=True,
+                )
+                
+        
+            else :
+
+                idf = df[["Vn1 (V)", f" |In1(A)| diode_{corner}.{i}"]].copy()
+                idf.rename(
+                    columns={
+                        f" |In1(A)| diode_{corner}.{i}": "diode_measured",
+                        "Vn1 (V)" : "measured_volt"
+                    },
+                    inplace=True,
+                ) 
+
+
+            os.makedirs(f"{dev_path}/measured_iv", exist_ok=True)
+            idf.to_csv(f"{dev_path}/measured_iv/measured_A{width}_P{length}_t{temp}_{corner}.csv")
+            
+            leng.append(length)
+            wid.append(width)
+            tempr.append(temp)
+            cor.append(corner)
+            meas.append(f"{dev_path}/measured_iv/measured_A{width}_P{length}_t{temp}_{corner}.csv")
+
+        
+            sdf = {"length":leng,"width":wid,"temp":tempr,"corner":cor,"diode_measured":meas}
+            sdf = pd.DataFrame(sdf)
+            all_dfs.append(sdf)
+
+    
+    df = pd.concat(all_dfs)       
+    df.dropna(axis=0, inplace=True)
+    df["device"] = device
+    df = df[["device","length","width","temp","corner","diode_measured"]]
+
+    return df
+
+def run_sim(char,dirpath, device, length, width, corner, temp):
     """ Run simulation at specific information and corner """
-    netlist_tmp = "./device_netlists/cv.spice"
+    netlist_tmp = f"./device_netlists/{char}.spice"
 
     
 
@@ -166,13 +248,13 @@ def run_sim(dirpath, device, length, width, corner, temp):
     temp_str = "{:.1f}".format(temp)
     
 
-    netlist_path = f"{dirpath}/{device}_netlists_cv/netlist_A{width_str}_P{length_str}_t{temp_str}_{corner}.spice"
-    result_path = f"{dirpath}/simulated_cv/simulated_A{width_str}_P{length_str}_t{temp_str}_{corner}.csv"
+    netlist_path = f"{dirpath}/{device}_netlists_{char}/netlist_A{width_str}_P{length_str}_t{temp_str}_{corner}.spice"
+    result_path = f"{dirpath}/simulated_{char}/simulated_A{width_str}_P{length_str}_t{temp_str}_{corner}.csv"
     
     with open(netlist_tmp) as f:
         tmpl = Template(f.read())
-        os.makedirs(f"{dirpath}/{device}_netlists_cv", exist_ok=True)
-        os.makedirs(f"{dirpath}/simulated_cv", exist_ok=True)
+        os.makedirs(f"{dirpath}/{device}_netlists_{char}", exist_ok=True)
+        os.makedirs(f"{dirpath}/simulated_{char}", exist_ok=True)
 
         with open(netlist_path, "w") as netlist:
             netlist.write(
@@ -201,7 +283,7 @@ def run_sim(dirpath, device, length, width, corner, temp):
     return info
 
 
-def run_sims(df, dirpath, num_workers=mp.cpu_count()):
+def run_sims(char,df, dirpath, num_workers=mp.cpu_count()):
 
     results = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
@@ -210,6 +292,7 @@ def run_sims(df, dirpath, num_workers=mp.cpu_count()):
             futures_list.append(
                 executor.submit(
                     run_sim,
+                    char,
                     dirpath,
                     row["device"],
                     row["length"],
@@ -227,7 +310,7 @@ def run_sims(df, dirpath, num_workers=mp.cpu_count()):
                 print("Test case generated an exception: %s" % (exc))
 
 
-    sf = glob.glob(f"{dirpath}/simulated_cv/*.csv") # stored simulated data files 
+    sf = glob.glob(f"{dirpath}/simulated_{char}/*.csv") # stored simulated data files 
     for i in range(len(sf)):
         sdf = pd.read_csv(
                 sf[i],
@@ -277,6 +360,8 @@ def main():
         "sc_diode",
     ]
 
+    char = ["cv","iv"]
+
     for i, dev in enumerate(devices):
         dev_path = f"{main_regr_dir}/{dev}"
 
@@ -288,87 +373,104 @@ def main():
         print("######" * 10)
         print(f"# Checking Device {dev}")
 
-        diode_cv_data_files = glob.glob(
-            f"./0_measured_data/{dev}_cv.nl_out.xlsx"
-        )
-        if len(diode_cv_data_files) < 1:
-            print("# Can't find mimcap file for device: {}".format(dev))
-            diode_cv_file = ""
-        else:
-            diode_cv_file = diode_cv_data_files[0]
-        print("# diode_cv data points file : ", diode_cv_file)
+        print("\n")
 
-        if diode_cv_file == "" :
-            print(f"# No datapoints available for validation for device {dev}")
-            continue
-            
-        if diode_cv_file != "":
-            meas_cv_df = ext_cv_measured(diode_cv_file, dev, corners,dev_path)
-        else:
-            meas_cv_df = []
-        
-        meas_len = len(pd.read_csv(glob.glob(f"{dev_path}/measured_cv/*.csv")[1]))
+        for c in char :
 
+            # cv section
 
-        print("# Device {} number of measured_datapoints : ".format(dev), len(meas_cv_df)*meas_len)
-        
-
-        sim_cv_df = run_sims(meas_cv_df, dev_path, 3)
-        sim_len = len(pd.read_csv(glob.glob(f"{dev_path}/simulated_cv/*.csv")[1]))
-        print("# Device {} number of simulated datapoints : ".format(dev), len(sim_cv_df)*sim_len)
-
-
-        merged_cv_df = meas_cv_df.merge(
-            sim_cv_df, on=["device", "corner", "length", "width", "temp"], how="left"
-        )
-        
-        merged_cv_dfs = []
-        for i in range(len(merged_cv_df)):
-            measured_cv_data = pd.read_csv(merged_cv_df["diode_measured"][i])
-            simulated_cv_data = pd.read_csv(merged_cv_df["diode_sim"][i])
-            result_cv_data = simulated_cv_data.merge(
-            measured_cv_data, how="left"
+            diode_data_files = glob.glob(
+                f"./0_measured_data/{dev}_{c}.nl_out.xlsx"
             )
-            result_cv_data["corner"] = merged_cv_df["diode_measured"][i].split("/")[-1].split("_")[-1].split(".")[0]
-            result_cv_data["device"] = merged_cv_df["diode_measured"][i].split("/")[1]
-            result_cv_data["length"] = merged_cv_df["diode_measured"][i].split("/")[-1].split("_")[1].split("A")[1]
-            result_cv_data["width"] = merged_cv_df["diode_measured"][i].split("/")[-1].split("_")[2].split("P")[1]
-            result_cv_data["temp"] = merged_cv_df["diode_measured"][i].split("/")[-1].split("_")[3].split("t")[1]
+            if len(diode_data_files) < 1:
+                print("# Can't find diode file for device: {}".format(dev))
+                diode_file = ""
+            else:
+                diode_file = diode_data_files[0]
+            print(f"# diode_{c} data points file : ", diode_file)
+
+
+            if diode_file == "" :
+                print(f"# No cv_datapoints available for validation for device {dev}")
+                continue
+
+            if "c" in c :
+                f = ext_cv_measured
+            elif "i" in c :
+                f = ext_iv_measured
+
+            if diode_file != "":
+                meas_df = f(diode_file, dev, corners,dev_path)
+            else:
+                meas_df = []
+
             
+            meas_len = len(pd.read_csv(glob.glob(f"{dev_path}/measured_{c}/*.csv")[1]))
+
+
+            print(f"# Device {dev} number of {c}_measured_datapoints : ", len(meas_df)*meas_len)
             
-            result_cv_data["error"] = (
-                np.abs(result_cv_data["diode_simulated"] - result_cv_data["diode_measured"])
-                 * 100.0
-                / result_cv_data["diode_measured"]
+
+            sim_df = run_sims(c,meas_df, dev_path, 3)
+            sim_len = len(pd.read_csv(glob.glob(f"{dev_path}/simulated_{c}/*.csv")[1]))
+            print(f"# Device {dev} number of {c}_simulated datapoints : ", len(sim_df)*sim_len)
+
+            # compare section 
+
+         
+
+            merged_df = meas_df.merge(
+                sim_df, on=["device", "corner", "length", "width", "temp"], how="left"
+            )
+            
+            merged_dfs = []
+            for i in range(len(merged_df)):
+                measured_data = pd.read_csv(merged_df["diode_measured"][i])
+                simulated_data = pd.read_csv(merged_df["diode_sim"][i])
+                result_data = simulated_data.merge(
+                measured_data, how="left"
+                )
+                result_data["corner"] = merged_df["diode_measured"][i].split("/")[-1].split("_")[-1].split(".")[0]
+                result_data["device"] = merged_df["diode_measured"][i].split("/")[1]
+                result_data["length"] = merged_df["diode_measured"][i].split("/")[-1].split("_")[1].split("A")[1]
+                result_data["width"] = merged_df["diode_measured"][i].split("/")[-1].split("_")[2].split("P")[1]
+                result_data["temp"] = merged_df["diode_measured"][i].split("/")[-1].split("_")[3].split("t")[1]
+                
+                
+                result_data["error"] = (
+                    np.abs(result_data["diode_simulated"] - result_data["diode_measured"])
+                    * 100.0
+                    / result_data["diode_measured"]
+                )
+
+                result_data = result_data [
+                    ["device","length","width","temp","corner","measured_volt","diode_measured","diode_simulated","error"]
+                ]
+
+                merged_dfs.append(result_data)
+            
+            merged_out = pd.concat(merged_dfs)
+
+
+            merged_out.to_csv(f"{dev_path}/error_analysis_cv.csv", index=False)
+
+            print(
+                "# Device {} min error: {:.2f} , max error: {:.2f}, mean error {:.2f}".format(
+                    dev,
+                    merged_out["error"].min(),
+                    merged_out["error"].max(),
+                    merged_out["error"].mean(),
+                )
             )
 
-            result_cv_data = result_cv_data [
-                ["device","length","width","temp","corner","measured_volt","diode_measured","diode_simulated","error"]
-            ]
+            if result_data["error"].max() < PASS_THRESH:
+                print("# Device {} has passed regression.".format(dev))
+            else:
+                print("# Device {} has failed regression. Needs more analysis.".format(dev))
 
-            merged_cv_dfs.append(result_cv_data)
-        
-        merged_cv_out = pd.concat(merged_cv_dfs)
+            print("\n\n")
 
-
-        merged_cv_out.to_csv(f"{dev_path}/error_analysis_cv.csv", index=False)
-
-        print(
-            "# Device {} min error: {:.2f} , max error: {:.2f}, mean error {:.2f}".format(
-                dev,
-                merged_cv_out["error"].min(),
-                merged_cv_out["error"].max(),
-                merged_cv_out["error"].mean(),
-            )
-        )
-
-        if result_cv_data["error"].max() < PASS_THRESH:
-            print("# Device {} has passed regression.".format(dev))
-        else:
-            print("# Device {} has failed regression. Needs more analysis.".format(dev))
-
-        print("\n\n")
-        
+            
 
 
 # # ================================================================
